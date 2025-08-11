69b62300898f35df6c23ea8b2976eca5778ce280
Ana Paula Oliveira <paulaoliveiraprates28@gmail.com>
2025-08-11 18:17:43 +0000

    atualização do crud da tabela livro

--- crud/categoria.py
+++ crud/categoria.py
@@ -1,27 +1,37 @@
-fro db_config import conectar
+from db_config import conectar
 
 def criar_categoria(nome,descricao):
-    conn = conectar()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO Categoria (nome, descricao)VALUES(%s, %s)", (nome, descricao))
-    conn.commit()
-    conn.close()
-    print("categoria criada com sucesso!")
+        try:
+        conn = conectar()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Categoria (nome, descricao)VALUES(%s, %s)", (nome, descricao))
+        conn.commit()
+        return{"status": "sucesso", "mensagem":"categora criada com sucesso!"}
+    except Exception as e:
+        return{"status":"erro","mensagem":}
+    finally:
+        conn.close()
+
 
 def listar_categoria(nome, descricao):
-    conn = conectar()
-    cursor = conn.cursor()
-    cursor.execute("SELECT *FROM Categoria")
-    caregoria = cursor.fetchall()
-    conn.close()
-    return categorias
+    try:
+        conn = conectar()
+        cursor = conn.cursor()
+        cursor.execute("SELECT *FROM Categoria")
+        return{""}
+        except Exception as e:
+            return{""}
+        finally:
+            conn.close()
 
 def atualizar_categoria(id_categoria, novo_nome, nova_descricao):
+    try:
     conn = conectar()
     cursor = conn.cursor()
     cursor.execute("UPDATE categoria SET nome=%s, descricao=%s where id=%s",
                     (novo_nome, nova_descricao, id_categoria))
     conn.commit()
+
     conn.close()
     print("categoria atualizada")
 
@@ -36,4 +46,4 @@ def deletar_categoria(id_categoria, novo_nome, nova_descricao):
             return{"status": "aviso", "mensagem":"Nenhuma categoria encontrada para atualizar."}
         return{"status": "sucesso", "mensagem":"Categoria atualizada!" }
     except Exception as e:
-        return{"status":}
+        return{"status": "erro", "mensagen":"Nenhuma categoria encontrada para atualizar."}
