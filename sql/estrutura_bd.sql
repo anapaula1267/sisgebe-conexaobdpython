91f240e16ef7b7c4f209979a88a0ce5dd938659a
Ana Paula Oliveira <paulaoliveiraprates28@gmail.com>
2025-08-15 11:34:59 +0000

    criação de códigos html

--- sql/estrutura_bd.sql
+++ sql/estrutura_bd.sql
@@ -1,17 +1,17 @@
--- Criação do banco de dados paragerenciar uma biblioteca escolar - SGB
+-- Criação do banco de dados para gerenciar uma biblioteca escolar - SGB
 CREATE DATABASE IF NOT EXISTS SGB;
 USE SGB;
 
 -- Tabela Categoria
 CREATE TABLE Categoria (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     descricao TEXT
-):
+);
 
 -- Tabela Livro
-CREATE TABLE Livro(
-    id int PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE livro (
+    id INT PRIMARY AUTO_INCREMENT,
     titulo VARCHAR(150) NOT NULL,
     autor VARCHAR(100) NOT NULL,
     isbn VARCHAR(20) UNIQUE,
@@ -19,21 +19,21 @@ CREATE TABLE Livro(
     capa TEXT,
     quantidade INT DEFAULT 1,
     Categoria_id INT,
-    FOREING KEY(Categoria_id) REFERENCES Categoria(id)
+    FOREIGN KEY (Categoria_id) REFERENCES Categoria(id)
 );
 
 -- Tabela Aluno
-CREATE TABLE Aluno(
+CREATE TABLE Aluno (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
-    serio VARCHAR(20) NOT NULL,
+    serie VARCHAR(20) NOT NULL,
     satus ENUM('ativo', 'bloqueado') DEFAULT 'ativo'
 );
 
 -- Tabela Professor
-CREATE TABLE Professor(
+CREATE TABLE Professor (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -42,8 +42,8 @@ CREATE TABLE Professor(
     satus ENUM('ativo', 'inativo') DEFAULT 'ativo'
 );
 
---Tabela BIbliotecario
-CREATE TABLE BIbliotecario(
+-- Tabela Bibliotecario
+CREATE TABLE Bibliotecario (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -51,7 +51,7 @@ CREATE TABLE BIbliotecario(
     satus ENUM('ativo', 'inativo') DEFAULT 'ativo'
 );
 
---Tabela Diretor
+-- Tabela Diretor
 CREATE TABLE Diretor(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
@@ -60,74 +60,74 @@ CREATE TABLE Diretor(
     satus ENUM('ativo', 'inativo') DEFAULT 'ativo'
 );
 
---Tabela Supervisor
-CREATE TABLE Supervisor(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+-- Tabela Supervisor
+CREATE TABLE Supervisor (
+ id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
-    senha VARCHAR(255) NOT NULL
-    satus ENUM('Ativo', 'inativo') DEFAULT 'ativo'
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    satus ENUM('ativo', 'inativo') DEFAULT 'ativo'
 );
 
---Tabela Emprestimo
-CREATE TABLE Emprestimo(
+-- Tabela Emprestimo
+CREATE TABLE Emprestimo (
     id INT PRIMARY KEY AUTO_INCREMENT,
     aluno_id INT,
     livro_id INT,
     data_emprestimo DATE NOT NULL,
     data_devolucao_prevista DATE,
-    data_devolucao_real DATE,
+    data_devolucao_real DATE, 
     multa DECIMAL(6,2) DEFAULT 0.00,
-    FOREIGN KEY (aluno_id) REFERENCES Aluno(id),
-    FOREIGN KEY (livro_id) REFERENCES livro(id)
-);
-
---Tabela Reserva
-CREATE TABLE Reserva(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    aluno_id INT,
-    livro_id INT,
-    data_reserva DATE,
-    satus ENUM('ativa', 'expirada', 'cancelada')
-    FOREIGN KEY (aluno_id) REFERENCES Aluno(id),
-    FOREIGN KEY (livro_id) REFERENCES livro(id)
-);
-
---Tabela HistoricoLeitura
-CREATE TABLE HistoricoLeitura(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    aluno_id INT,
-    livro_id INT,
-    data_inicio DATE,
-    data_fim DATe,
     FOREIGN KEY (aluno_id) REFERENCES Aluno(id),
     FOREIGN KEY (livro_id) REFERENCES Livro(id)
 );
 
---Tabela Sugestao
-CREATE TABLE Sugestao(
+-- Tabela Reserva 
+CREATE TABLE Reserva (
+     id INT PRIMARY KEY AUTO_INCREMENT,
+    aluno_id INT,
+    livro_id INT,
+    data_reserva DATE,
+    satus ENUM('ativa', 'expirada', 'cancelada'),
+    FOREIGN KEY (aluno_id) REFERENCES Aluno(id),
+    FOREIGN KEY (livro_id) REFERENCES Livro(id)
+);
+
+-- Tabela HistoricoLeitura
+CREATE TABLE HistoricoLeitura (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    aluno_id INT,
+    livro_id INT,
+    data_inicio DATE,
+    data_fim DATE,
+    FOREIGN KEY (aluno_id) REFERENCES Aluno(id),
+    FOREIGN KEY (livro_id) REFERENCES Livro(id)
+);
+
+-- Tabela Sugestao
+CREATE TABLE Sugestao (
     id INT PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(150),
     autor VARCHAR(100),
     categoria VARCHAR(50),
     justificativa TEXT,
     data_sugestao DATE,
-    alino_id INT,
+    aluno_id INT,
     professor_id INT,
     FOREIGN KEY (aluno_id) REFERENCES Aluno(id),
     FOREIGN KEY (professor_id) REFERENCES Professor(id)
 );
 
---Tabela Relatorio
-CREATE TABLE Relatorio(
+-- Tabela Relatorio
+CREATE TABLE Relatorio (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    tipo Enum ('mensal','turma,' 'aluno', 'livro'),
+    tipo ENUM('mensal', 'turma', 'aluno', 'livro'),
     periodo_inicio DATE,
     periodo_fim DATE,
     gerado_por_bibliotecario INT,
     gerado_por_diretor INT,
-    gerado_por_surpervisor INT,
-    FOREIGN KEY (gerado_por_bibliotecario) REFERENCES BIbliotecario(id),
+    gerado_por_supervisor INT,
+    FOREIGN KEY (gerado_por_bibliotecario) REFERENCES Bibliotecario(id),
     FOREIGN KEY (gerado_por_diretor) REFERENCES Diretor(id),
-    FOREIGN KEY (gerado_por_surpervisor) REFERENCES Supervisor(id)
+    FOREIGN KEY (gerado_por_supervisor) REFERENCES Supervisor(id)
 );
