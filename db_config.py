0c4ea6be7d8c57a18da2b6bee1414ceb48f5a4d4
Ana Paula Oliveira <paulaoliveiraprates28@gmail.com>
2025-08-27 19:39:59 +0000

    criação de arquivos atualização de codigo

--- db_config.py
+++ db_config.py
@@ -3,14 +3,34 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+
+# Carrega variáveis do .env
+load_dotenv()
+
+# Caminho do banco SQLite (arquivo dentro do projeto)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DB_PATH = os.path.join(BASE_DIR, "sgb.db")
 
 db = SQLAlchemy()
 
-# banco ficará no arquivo sgb.db na raiz do projeto
-DB_PATH = os.path.join(os.path.dirname(__file__), "sgb.db")
+def init_app(app: Flask):
+    db_type = os.getenv("DB_TYPE", "sqlite")  # sqlite é o padrão
 
-def init_app(app: Flask) -> Flask:
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
+    if db_type == "mysql":
+        user = os.getenv("DB_USER")
+        password = quote_plus(os.getenv("DB_PASSWORD", ""))
+        host = os.getenv("DB_HOST", "127.0.0.1")
+        port = os.getenv("DB_PORT", "3306")
+        dbname = os.getenv("DB_NAME", "sgb")
+
+        app.config["SQLALCHEMY_DATABASE_URI"] = (
+            f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{dbname}"
+        )
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
+
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     return app
